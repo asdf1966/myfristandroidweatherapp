c4d286d79f0d43349265336b284bb3149120ee09
asdf1966 <xw712@126.com>
2018-01-10 08:42:12 +0800

    无错误代码

--- app/src/main/java/com/example/administrator/myfristandroidweatherapp/db/County.java
+++ app/src/main/java/com/example/administrator/myfristandroidweatherapp/db/County.java
@@ -8,21 +8,20 @@ import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport {
     private  int id;
-    private  String countyame;
+    private  String countyName;
     private  String weatherId;
     private  int cityId;
     public int getId(){
         return  id;
     }
-
     public  void  setId(int  id){
         this.id=id;
     }
-    public  String getCountyame(){
-        return  countyame;
+    public  String getCountyName(){
+        return  countyName;
     }
-    public  void  setCountyame(String countyNmae){
-        this.countyame=countyNmae;
+    public  void  setCountyName(String countyName){
+        this.countyName=countyName;
     }
     public  String getWeatherId(){
         return  weatherId;
